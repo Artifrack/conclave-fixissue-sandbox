8fb2f8ab528fe0c33641bfc7e85bcee7b1eb8e6e
Conclave <conclave@localhost>
2026-07-31 06:18:23 +0000

    Fix #9: flatten_one_level does not actually flatten anything

--- flatten.py
+++ flatten.py
@@ -2,5 +2,5 @@ def flatten_one_level(list_of_lists):
     """Flatten a list of lists by exactly one level."""
     result = []
     for sublist in list_of_lists:
-        result.append(sublist)
+        result.extend(sublist)
     return result
